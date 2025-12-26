0eb5b23f6d28ea34dea2393a5b262466f4d4656b
Jahnavi-215 <tatajahnavi15@gmail.com>
2025-12-26 16:53:04 +0530

    Delete hirehand-backend directory

--- hirehand-backend/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.int.test.js'],
-  collectCoverage: false,
-  collectCoverageFrom: ['src/**/*.js'],
-  coverageReporters: ['text', 'lcov']
-};
